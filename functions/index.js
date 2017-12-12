3a81f53ea3a93186710635630b36fa2dde1c79a6
Ryan Magoon <mr.magoon5@gmail.com>
2017-12-11 22:49:20 -0500

    firebase poop;

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions')
+const createUser = require('./create_user')
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.goodbye = functions.https.onRequest((request, response) => {
+  response.send("Goodbye!")
+})
